b76d2b536ede234891762e5d86a37aca997f0937
adjiap <20769489+adjiap@users.noreply.github.com>
2024-11-17 16:27:18 +0100

    Corrected MainMenu and Pause uxml and cs function

--- Assets/DoppelEins/Technophobia/UI/MainMenuManager.cs
+++ Assets/DoppelEins/Technophobia/UI/MainMenuManager.cs
@@ -1,39 +1,48 @@
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class PauseManager : MonoBehaviour
+public class MainMenuManager : MonoBehaviour
 {
     public VisualElement ui;
-    public Button resumeGameButton;
+
+    public Button newGameButton;
     public Button optionButton;
+    public Button creditsButton;
     public Button quitGameButton;
 
-    private void Start()
+    private void Awake()
     {
         ui = GetComponent<UIDocument>().rootVisualElement;
     }
 
     private void OnEnable()
     {
-        resumeGameButton = ui.Q<Button>("ResumeButton");
-        resumeGameButton.clicked += OnResumeGameButtonClicked;
+        newGameButton = ui.Q<Button>("NewGameButton");
+        newGameButton.clicked += OnNewGameButtonClicked;
 
         optionButton = ui.Q<Button>("OptionButton");
         optionButton.clicked += OnOptionButtonClicked;
 
+        creditsButton = ui.Q<Button>("CreditsButton");
+        creditsButton.clicked += CreditsGameButtonClicked;
+        
         quitGameButton = ui.Q<Button>("QuitGameButton");
         quitGameButton.clicked += OnQuitGameButtonClicked;
     }
 
-    private void OnResumeGameButtonClicked()
+    private void OnNewGameButtonClicked()
     {
-        Debug.Log("Resume Button Clicked");
+        // TODO: change in the future, as this means we have the main game running in the background
+        Debug.Log("New Game Started");
     }
     private void OnOptionButtonClicked()
     {
         Debug.Log("Option Button Clicked");
+    }
+    private void CreditsGameButtonClicked()
+    {
+        Debug.Log("Credits Button Clicked");
     }
     private void OnQuitGameButtonClicked()
     {
